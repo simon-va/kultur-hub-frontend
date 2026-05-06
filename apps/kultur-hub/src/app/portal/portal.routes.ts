736c1f04033f226be5e347948e087d6577668d1b
Simon Vahlbrock <simon.vahlbrock@icloud.com>
2026-05-06 21:10:16 +0200

    Adds portal Overview

--- apps/kultur-hub/src/app/portal/portal.routes.ts
+++ apps/kultur-hub/src/app/portal/portal.routes.ts
@@ -1,16 +1,45 @@
 import { Routes } from '@angular/router';
+import { PortalShell } from '@kultur-hub/portal/ui';
 
 export const portalRoutes: Routes = [
   {
     path: '',
-    // TODO: add portal shell/layout component
+    component: PortalShell,
     children: [
-      { path: '', redirectTo: 'organizations', pathMatch: 'full' },
+      {
+        path: '',
+        pathMatch: 'full',
+        loadChildren: () =>
+          import('@kultur-hub/portal/feature-overview').then(
+            (m) => m.portalFeatureOverviewRoutes
+          ),
+      },
       {
         path: 'organizations',
         loadChildren: () =>
           import('@kultur-hub/portal/feature-organizations').then(
             (m) => m.portalFeatureOrganizationsRoutes
+          ),
+      },
+      {
+        path: 'events',
+        loadChildren: () =>
+          import('@kultur-hub/portal/feature-events').then(
+            (m) => m.portalFeatureEventsRoutes
+          ),
+      },
+      {
+        path: 'reports',
+        loadChildren: () =>
+          import('@kultur-hub/portal/feature-reports').then(
+            (m) => m.portalFeatureReportsRoutes
+          ),
+      },
+      {
+        path: 'profile',
+        loadChildren: () =>
+          import('@kultur-hub/portal/feature-profile').then(
+            (m) => m.portalFeatureProfileRoutes
           ),
       },
     ],
---
+++ libs/portal/feature-overview/src/lib/lib.routes.ts
@@ -0,0 +1,6 @@
+import { Route } from '@angular/router';
+import { PortalFeatureOverview } from './portal-feature-overview/portal-feature-overview';
+
+export const portalFeatureOverviewRoutes: Route[] = [
+  { path: '', component: PortalFeatureOverview },
+];
