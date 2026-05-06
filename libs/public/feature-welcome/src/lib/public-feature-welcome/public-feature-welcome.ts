00043e331437710e3b3ee9e20c7051e474f14f03
Simon Vahlbrock <simon.vahlbrock@icloud.com>
2026-05-06 18:19:26 +0200

    Adds welcome page

--- libs/public/feature-welcome/src/lib/public-feature-welcome/public-feature-welcome.ts
+++ libs/public/feature-welcome/src/lib/public-feature-welcome/public-feature-welcome.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'lib-public-feature-welcome',
-  imports: [],
+  imports: [ButtonModule, RouterLink],
   templateUrl: './public-feature-welcome.html',
   styleUrl: './public-feature-welcome.scss',
 })
